0e13ce3ba33cf85d61fe736cf5118f39f996d68f
monishstark <monishkumarms3@gmail.com>
2022-03-30 15:02:47 +0530

    update

--- src/Home.js
+++ src/Home.js
@@ -169,38 +169,38 @@ function Home() {
         <div className="home_row">
           <Product
             id="00015"
-            title="Puma"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/4/22/ef9c71f4-3cef-4d28-bf50-9d177a4080501619092516324-1.jpg"
-            price={2500}
-            info="Puma Hoodies"
+            title="Adidas Original"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17155272/2022/3/8/2a907940-01cb-4a07-beb6-181bfe85cb021646731760097-ADIDAS-Originals-Men-Casual-Shoes-5491646731759857-1.jpg"
+            price={8000}
+            info="Men Stan Smith South Park"
           />
           <Product
             id="00016"
-            title="Wrogn"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12422882/2020/11/24/ed3e6a50-4b2e-478f-9f21-8deed67169311606199218735SweatshirtsWROGNMenSweatshirtsWROGNMen1.jpg"
-            price={1800}
-            info="Wrogn Hoodies"
+            title="Puma"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14825960/2021/9/6/307a247d-2796-4301-87ed-b1e5d6dc18bf1630917956986-PUMA-Motorsport-Unisex-Red-Scuderia-Ferrari-A3ROCAT-Mid-Snea-3.jpg"
+            price={8000}
+            info="Puma X Ferrari"
           />
           <Product
             id="00017"
-            title="H&M"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17124218/2022/2/10/8ef47f19-77c0-46fe-bf5e-6e9718c9432c1644513034501RelaxedFitPrintedhoodie1.jpg"
-            price={1500}
-            info="H&M Hoodies"
+            title="Nike"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17206048/2022/3/15/617aa5fc-b189-4439-b8bd-d1022d42b8851647347503397-Nike-Court-Legacy-Mens-Shoes-3251647347503147-5.jpg"
+            price={4500}
+            info="Court Legacy"
           />
           <Product
             id="00018"
-            title="H&M"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17124324/2022/2/10/e00d3a88-c6f3-4c24-b164-8838994aa1581644507935627OversizedFitCottonzip-throughhoodie1.jpg"
-            price={3000}
-            info="H&M Hoodies"
+            title="Adidas"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17155290/2022/3/8/cf170350-dbae-4a3f-8bab-b618eed555c91646722469734-ADIDAS-Unisex-Sports-Shoes-9321646722469359-2.jpg"
+            price={3800}
+            info="Speedflow Football Studs"
           />
           <Product
             id="00019"
             title="Adidas"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14746186/2021/12/6/304b4043-b135-45ae-b551-01bfde6606bb1638786590308-ADIDAS-Men-Sweatshirts-5121638786589794-1.jpg"
-            price={3000}
-            info="Adidas Hoodies"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17145990/2022/3/23/eb778f5d-737e-41e7-aeb5-1060153e69e01648027756551-ADIDAS-Unisex-Sports-Shoes-4431648027756212-1.jpg"
+            price={5000}
+            info="Adidas Predator"
           />
         </div>
         <div className="hovering">
@@ -209,38 +209,38 @@ function Home() {
         <div className="home_row">
           <Product
             id="00020"
-            title="Puma"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/4/22/ef9c71f4-3cef-4d28-bf50-9d177a4080501619092516324-1.jpg"
-            price={2500}
-            info="Puma Hoodies"
+            title="Highlander"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2347551/2019/12/3/fb88c67b-d735-48cc-8554-9374a95ba35b1575363237983-HIGHLANDER-Men-White--Light-Blue-Slim-Fit-Striped-Casual-Shi-1.jpg"
+            price={540}
+            info="Men Slim Fit Shirt"
           />
           <Product
             id="00021"
             title="Wrogn"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12422882/2020/11/24/ed3e6a50-4b2e-478f-9f21-8deed67169311606199218735SweatshirtsWROGNMenSweatshirtsWROGNMen1.jpg"
-            price={1800}
-            info="Wrogn Hoodies"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11361160/2020/2/24/6e48c2a4-5d44-48a1-9e89-ef45e60a6a671582535979796-WROGN-Men-Shirts-6061582535978238-1.jpg"
+            price={1000}
+            info="Men Slim Fit Shirt"
           />
           <Product
             id="00022"
-            title="H&M"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17124218/2022/2/10/8ef47f19-77c0-46fe-bf5e-6e9718c9432c1644513034501RelaxedFitPrintedhoodie1.jpg"
-            price={1500}
-            info="H&M Hoodies"
+            title="Roadster"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10092097/2019/10/15/adebe2b5-fa5a-4f24-aa6b-318a220756c21571136170029-Roadster-Men-Jeans-2931571136167945-1.jpg"
+            price={700}
+            info="Skinny Fit Jeans"
           />
           <Product
             id="00023"
-            title="H&M"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17124324/2022/2/10/e00d3a88-c6f3-4c24-b164-8838994aa1581644507935627OversizedFitCottonzip-throughhoodie1.jpg"
-            price={3000}
-            info="H&M Hoodies"
+            title="Peter England"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16616792/2022/1/19/e48e8c04-7182-42a9-90e4-2f263a239db51642584885366-Peter-England-Casuals-Men-Blue-Skinny-Fit-Mid-Rise-Light-Fad-1.jpg"
+            price={1200}
+            info="Peter England Jeans"
           />
           <Product
             id="00024"
-            title="Adidas"
-            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14746186/2021/12/6/304b4043-b135-45ae-b551-01bfde6606bb1638786590308-ADIDAS-Men-Sweatshirts-5121638786589794-1.jpg"
-            price={3000}
-            info="Adidas Hoodies"
+            title="Levi's"
+            image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16612822/2022/1/17/e9f9c01d-5074-4b5f-a1a2-a6ae44dffe991642412448392-Levis-Men-Blue-512-Slim-Fit-Mildly-Distressed-Light-Fade-Str-1.jpg"
+            price={1300}
+            info="Skinny Fit Jeans"
           />
         </div>
 
